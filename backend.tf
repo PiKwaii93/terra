8d5368c29f24bb4cc932311ca02aca0124365876
maxence.nho <maxence.nho@hetic.net>
2024-03-13 15:50:07 +0100

    init 3

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "github-actions-demo-9876"
-    key    = "github-actions-demo.tfstate"
-    region = "us-east-1"
+    bucket = "maxterratest"
+    key    = "arn:aws:kms:us-west-2:848651146133:key/e440acdb-bbf2-4daa-9e9b-c7f35bcdf83c"
+    region = "us-west-2"
   }
 }
